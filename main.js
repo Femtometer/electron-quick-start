bf670542aaf56cad7a72c0007f22fd20794e0e1e
Tension <admin@striverbar.com>
2016-06-22 18:50:23 +0800

    更新Electron版本
    V1.2.3

--- main.js
+++ main.js
@@ -4,13 +4,17 @@ const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
+app.on('close-main-window', function(){
+  app.quit()
+})
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 600, height: 400, resizable: false, frame: false})
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
--- renderer.js
+++ renderer.js
@@ -1,3 +1,7 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+function end_main(){
+    var ipc = window.require('ipc')
+    ipc.send('close-main-window')
+}
