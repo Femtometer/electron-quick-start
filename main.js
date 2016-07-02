039f2cc7f146375762f0b08d915b9c24593945eb
Tension <admin@striverbar.com>
2016-07-02 23:13:30 +0800

    测试IPC按键触发

--- main.js
+++ main.js
@@ -1,40 +1,52 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+"use strict";
 
-app.on('close-main-window', function(){
-  app.quit()
+const electron = require('electron');
+const {contentTracing} = require('electron');
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+const ipcMain = electron.ipcMain;
+
+const options = {
+  categoryFilter: '*',
+  traceOptions: 'record-until-full,enable-sampling'
+}
+
+contentTracing.startRecording(options, () => {
+  console.log('Tracing started');
+  setTimeout(() => {
+    contentTracing.stopRecording('', (path) => {
+      console.log('Tracing data recorded to ' + path);
+    })
+  }, 5000);
 })
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let win;
 
 function createWindow () {
+  console.log('Create Main Window');
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 600, height: 400, resizable: false, frame: false})
-
+  win = new BrowserWindow({width: 600, height: 400, resizable: false, frame: false})
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
-
+  win.loadURL(`file://${__dirname}/index.html`);
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
+  win.webContents.openDevTools();
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  win.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    win = null
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -48,9 +60,20 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+  console.log('Open and activate')
+  if (win === null) {
     createWindow()
   }
+})
+
+app.on('close-main-window', function () {
+  console.log('Exit triged.')
+  app.quit()
+})
+
+ipcMain.on('show-dialog', (event, status) => {
+  console.log(status);
+  console.log('IPC data recevied');
 })
 
 // In this file you can include the rest of your app's specific main process
--- renderer.js
+++ renderer.js
@@ -1,7 +1,23 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-function end_main(){
-    var ipc = window.require('ipc')
-    ipc.send('close-main-window')
+var fs = require('fs')
+
+function end_main () {
+    console.log('Click_button');
+    document.write('Fun Called');
+    var ipc = window.require('ipc');
+    ipc.send('close-main-window');
+}
+
+function openFile () {
+    dialog.showOpenDialog({ filters: [
+    { name: 'text', extensions: ['txt'] }
+    ]}, function (fileNames) {
+        if (fileNames === undefined) return;
+        var fileName = fileNames[0];
+        fs.readFile(fileName, 'utf-8', function (err, data) {
+            document.getElementById("editor").value = data;
+        })
+    })
 }
